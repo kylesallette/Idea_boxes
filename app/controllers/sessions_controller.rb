13ccf5b1e0310721012d03251d30a04f19de5d6f
kylesallette <kylesallette@gmail.com>
2018-04-01 21:16:04 -0600

    sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationControllers
+
+  def new
+
+  end
+
+  def create
+    @user = User.find_by(name: params[:name])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, :notice => "successfully logged out"
+  end
+
+
+
+
+
+end
