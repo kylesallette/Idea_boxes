76ec1e1affaf3d6b2fbea817a52d8f08c2a6e7fd
kylesallette <kylesallette@gmail.com>
2018-04-01 21:13:51 -0600

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,23 @@ class UsersController < ApplicationController
    @user = User.new
   end
 
+  def show
+   @user = User.find(params[:id])
+  end
 
+ def create
+   @user = User.new(user_params)
+   if @user.save
+     redirect_to user_path(@user)
+   else
+     render :new
+   end
+  end
 
+ private
+
+ def user_params
+   params.require(:user).permit(:name, :password, :email)
+ end
+ 
 end
