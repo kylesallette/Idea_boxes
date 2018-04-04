b0ba80d645f4d75637c0c0808b294df09a1772e1
kylesallette <kylesallette@gmail.com>
2018-04-03 20:33:07 -0600

    adding users actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,31 @@
 class UsersController < ApplicationController
 
-
   def new
-   @user = User.new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
 
   def show
-   @user = User.find(params[:id])
+    @user = User.find(params[:id])
+    redirect_to root_path unless @user == current_user
   end
 
- def create
-   @user = User.new(user_params)
-   if @user.save
-     redirect_to user_path(@user)
-   else
-     render :new
-   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :email)
   end
 
- private
 
- def user_params
-   params.require(:user).permit(:name, :password, :email)
- end
- 
 end
