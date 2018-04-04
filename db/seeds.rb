f94b6b182809bb31a3a2da48eb2079df7f2c1f3b
kylesallette <kylesallette@gmail.com>
2018-04-03 20:34:46 -0600

    adding image and categories seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,20 @@ User.create!(name:  "admin",
              email: "kylesallette@gmail.com",
              password:              "red",
              role: 1)
+
+
+
+Image.create!(name: "sun", url: "https://image.ibb.co/eFnVux/images_1.jpg" )
+Image.create!(name: "neon-car", url: "https://image.ibb.co/jJGqux/images_3.jpg")
+Image.create!(name: "cat", url: "https://image.ibb.co/gUztEx/images_4.jpg" )
+Image.create!(name: "tv-watching", url: "https://image.ibb.co/nAQ0ux/images_5.jpg" )
+Image.create!(name: "man-sunset", url: "https://image.ibb.co/gBcVux/images.jpg" )
+
+
+
+Category.create!(title: "to-do")
+Category.create!(title: "art")
+Category.create!(title: "jobs")
+Category.create!(title: "comics")
+Category.create!(title: "travel")
+Category.create!(title: "home-work")
