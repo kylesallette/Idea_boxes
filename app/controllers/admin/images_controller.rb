21b6f46b82ca4decf6216aa79313d72453eb78eb
kylesallette <kylesallette@gmail.com>
2018-04-03 20:31:45 -0600

    adding images controller

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -1,11 +1,40 @@
 class Admin::ImagesController < Admin::BaseController
 
-  
 
+  def index
+    @images = Image.all
+  end
+
+  def edit
+    @image = Image.find(params[:id])
+  end
+
+  def destroy
+    image = Image.find(params[:id])
+    image.destroy
+    redirect_to admin_images_path
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+
+  def new
+   @image = Image.new
+ end
+
+ def create
+   @image = Image.create(image_params)
+   redirect_to admin_images_path
+
+ end
 
   private
 
   def image_params
-    params.require(:image).permit(:image_url)
+    params.require(:image).permit(:name, :url)
   end
+
+
 end
