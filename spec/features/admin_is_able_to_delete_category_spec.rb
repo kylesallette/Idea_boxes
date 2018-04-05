c3a34cd442e8c9c5360a2d3af6829e780e1827f1
kylesallette <kylesallette@gmail.com>
2018-04-04 19:09:53 -0600

    admin delete

---
+++ spec/features/admin_is_able_to_delete_category_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+  describe 'admin logs in'  do
+    context 'admin logs into app'  do
+      it 'admin navigates to add category and creates new category'  do
+        user = User.create(name: "admin", email: "something", password: "red", role: 1)
+
+        visit root_path
+
+        page.find("#have-account").click
+
+        fill_in("User Name*", with: "admin")
+        fill_in("Password*", with: "red")
+
+        click_button("Log In")
+
+        page.find("#add-cat").click
+
+        fill_in("new category*", with: "something fun")
+
+        click_button("Create Category")
+
+        click_link('Delete')
+
+        expect(page).to_not have_content("something fun")
+      end
+    end
+  end
