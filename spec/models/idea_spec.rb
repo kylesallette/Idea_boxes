364a01f03d8f0beff558d4dc87f19b84c782916d
kylesallette <kylesallette@gmail.com>
2018-04-04 19:25:39 -0600

    fixed spec test

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,5 +1,19 @@
 require "rails_helper"
 
   describe Idea, type: :model do
+      describe "validations" do
     it {should validate_presence_of(:name)}
   end
+
+   describe "relationships" do
+      it {is_expected.to have_many(:images)}
+    end
+
+    describe "relationships" do
+       it {is_expected.to have_many(:idea_images)}
+     end
+
+      describe "relationships" do
+         it {is_expected.to belong_to(:user)}
+       end
+     end
