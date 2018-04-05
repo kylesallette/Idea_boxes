0e3ca436b25a8e05db59075ac081a057227c1236
kylesallette <kylesallette@gmail.com>
2018-04-04 19:09:19 -0600

    adding admin test

--- spec/features/admin_is_able_to_add_category_spec.rb
+++ spec/features/admin_is_able_to_add_category_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
   describe 'admin logs in'  do
     context 'admin logs into app'  do
       it 'admin navigates to add category and creates new category'  do
+        user = User.create(name: "admin", email: "something", password: "red", role: 1)
+
         visit root_path
 
         page.find("#have-account").click
@@ -11,7 +13,7 @@ require 'rails_helper'
         fill_in("Password*", with: "red")
 
         click_button("Log In")
-    
+
         page.find("#add-cat").click
 
         fill_in("new category*", with: "something fun")
