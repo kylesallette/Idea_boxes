14e9c90510d8ded66f1a865531a4ec82dc1a9985
kylesallette <kylesallette@gmail.com>
2018-04-04 19:25:52 -0600

    fixxed test

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-  describe "relationships" do
-    it {is_expected.to have_many(:idea)}
+describe Image, type: :model do
+  describe "validations" do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:url)}
   end
+
+    describe "relationships" do
+      it {is_expected.to have_many(:ideas)}
+  end
+  describe "relationships" do
+    it {is_expected.to have_many(:idea_images)}
+  end
+end
