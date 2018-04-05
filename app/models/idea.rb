a5f08d49b49635cc9da5039cce457ae187bb437b
kylesallette <kylesallette@gmail.com>
2018-04-04 19:26:15 -0600

    fixed test

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,7 +1,7 @@
 class Idea < ApplicationRecord
   validates :name, :content, presence: true
-    belongs_to :user
     belongs_to :category, optional: true
+    belongs_to :user
 
     has_many :idea_images, dependent: :nullify
     has_many :images, through: :idea_images
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,6 @@
 class Image < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :url
 
   has_many :idea_images, dependent: :nullify
   has_many :ideas, through: :idea_images, class_name: "::Idea"
